e3ecb1a8ec981f926513c995c88b9eda37b518f2
Marshall <kamil.budzik98@gmail.com>
2020-05-11 12:51:03 +0200

    initial commit

---
+++ app/src/main/java/com/example/ppsm_budzik_shoutbox/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ppsm_budzik_shoutbox.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
